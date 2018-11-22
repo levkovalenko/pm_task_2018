b8a1ed1cebde431e6cd93ce2d3a16824f14f81f8
korwin <levozavr@mail.ru>
2018-11-22 05:09:03 +0300

    update gitignore

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from matrix_of_words import prior, words_w, ast,weights
 import math
 import operator
 answe = 0
-for k in range(50,60):
-    with open(f"lemm_text_clear_1000/{k}.txt") as f:
+with open("answer.txt", 'w') as ans:
+    with open(f"test_clear/test.txt") as f:
 
         for line in f:
             article = ast.literal_eval(line)
-            text = article[1].split(' ') + article[2].split(' ')
+            text = article[0].split(' ') + article[1].split(' ')
             score = dict()
             V = set()
             [V.update(set(x)) for x in words_w.values()]
@@ -27,11 +27,5 @@ for k in range(50,60):
                         #print(weights[i][j])
                         score[i] += -math.log(weights[i][j])
             #print(article[0], max(score.items(), key=operator.itemgetter(1))[0])
-            answe += 1 if article[0] == max(score.items(), key=operator.itemgetter(1))[0] else 0
-            #'''
-            if article[0] != max(score.items(), key=operator.itemgetter(1))[0]:
-                print(max(score.items(), key=operator.itemgetter(1))[0], article[0])
-                print(article[1])
-                print(score)
-            #'''
-print(f"{answe/100}%")
+            ans.write(f"{max(score.items(), key=operator.itemgetter(1))[0]}\n")
+
